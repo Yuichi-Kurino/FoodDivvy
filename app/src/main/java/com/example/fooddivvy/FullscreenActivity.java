c7b5e3a4b367e8ed4ccde7b6c0df5e56456fc46e
YuyusanK <kurinoyuichi@gmail.com>
2019-06-01 15:43:15 -0700

    added functions to the map button

--- app/src/main/java/com/example/fooddivvy/FullscreenActivity.java
+++ app/src/main/java/com/example/fooddivvy/FullscreenActivity.java
@@ -1,18 +1,22 @@
 package com.example.fooddivvy;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
 public class FullscreenActivity extends AppCompatActivity {
+    private Button button;
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -106,7 +110,19 @@ public class FullscreenActivity extends AppCompatActivity {
         // operations to prevent the jarring behavior of controls going away
         // while interacting with the UI.
         findViewById(R.id.map_button).setOnTouchListener(mDelayHideTouchListener);
+        button = findViewById(R.id.map_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openMainActivity();
+            }
+        });
     }
+    public void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
